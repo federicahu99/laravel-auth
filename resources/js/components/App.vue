7e732cd838a618e94c1ff314e0e7624745ba5012
feddericahu99 <federica.hu339@gmail.com>
2022-09-26 16:55:44 +0200

    connected vue

---
+++ resources/js/components/App.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="content">
+        <div class="title m-b-md">We Are Coming Soon...</div>
+    </div>
+</template>
+
+<script>
+    export default {
+       name:"App",
+    }
+</script>
