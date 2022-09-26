892f5cb74d33153917becaf9602515c8b7635b03
feddericahu99 <federica.hu339@gmail.com>
2022-09-26 16:36:52 +0200

    guest and admin any

--- routes/web.php
+++ routes/web.php
@@ -13,24 +13,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('guest.home');
-});
-
-Auth::routes(['register'=> false]);
+Auth::routes(['register' => false]);
 
 Route::get('/admin', 'Admin\HomeController@index')->middleware('auth')->name('admin.home');
 
 //amministrazione
-Route::middleware('auth') //
+Route::middleware('auth')
     ->prefix('admin') //ulr iniziale
     ->namespace('Admin') //controller folder
     ->name('admin.') // aggiunge al nome
     ->group(function () {
 
-    // Admin
-    Route::get('/', 'HomeController@index')->name('home');
+        // Admin
+        Route::get('/', 'HomeController@index')->name('home');
 
-    // Posts
-    // Route::resource('posts','PostController');
-});
+        // Posts
+        // Route::resource('posts','PostController');
+
+        Route::get('/{any}', function () { //qualunque cosa crivo dopo /admin mi manda alla 404
+            abort('404');
+        })->where('any', '.*');
+    });
+
+Route::get('/{any?}', function () { //qualunque cosa mi manda in home
+    return view('guest.home');
+})->where('any', '.*');
