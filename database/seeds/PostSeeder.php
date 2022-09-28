a469f7b994912e6254fca15270c368ff27cff6a1
feddericahu99 <federica.hu339@gmail.com>
2022-09-28 20:00:40 +0200

    random category

--- database/seeds/PostSeeder.php
+++ database/seeds/PostSeeder.php
@@ -1,9 +1,11 @@
 <?php
 
 use App\Models\Post;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
+use Illuminate\Support\Arr;
 
 class PostSeeder extends Seeder
 {
@@ -14,11 +16,15 @@ class PostSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
+
+        $number_of_categories = Category::pluck('id');
+
         for ($i =  0 ; $i < 10; $i++) {
             
             $new_post = new Post();
 
             $new_post->title = $faker->text(20);
+            $new_post->category_id = Arr::random($number_of_categories);
             $new_post->slug = Str::slug($new_post->title, '-');
             $new_post->content = $faker->paragraphs(2, true);
             $new_post->image = $faker->imageUrl(250, 250);
