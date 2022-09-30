615df667814dd057c667bf4d69e89320107d0f04
feddericahu99 <federica.hu339@gmail.com>
2022-09-30 16:17:19 +0200

    fixed on

--- database/migrations/2022_09_30_140222_create_post_tag_table.php
+++ database/migrations/2022_09_30_140222_create_post_tag_table.php
@@ -16,9 +16,9 @@ class CreatePostTagTable extends Migration
         Schema::create('post_tag', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('post_id');
-            $table->foreign('post_id')->references('id')->on('post')->onDelete('cascade');
+            $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
             $table->unsignedBigInteger('tag_id');
-            $table->foreign('tag_id')->references('id')->on('tag')->onDelete('cascade');
+            $table->foreign('tag_id')->references('id')->on('tags')->onDelete('cascade');
             $table->timestamps();
         });
     }
